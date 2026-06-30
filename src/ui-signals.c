fruviad/pkangband

commit e4ada7b1410f1862deb43fe08fadf4f6f773d24f

Rework SIGTSTP handling
Have most main.c front ends use the default handling and simply stop.  For the Curses, SDL, and SDL2 front ends, have the signal handler set a flag which is then tested during normal execution when Term_xtra(TERM_XTRA_EVENT, ...) is called.  If the flag is on, perform what had previously been done in the SIGTSTP handler, now in signals_perform_deferred_suspend().  Resolves https://github.com/angband/angband/issues/6612 .  The SDL and SDL2 front ends only have special handling of SIGTSTP so they can withdraw from fullscreen mode when stopping and then restore fullscreen when the process resumes.

Replace signals_ignore_tstp() and signals_handle_tstp() with signals_protect().  Besides SIGTSTP, also temporarily disable SIGINT, SIGQUIT, and SIGTERM's normal handling while in the process of writing the save or high score file.  If available, have signals_protect() use sigprocmask() rather than changing signal handlers.  Resolves https://github.com/angband/angband/issues/6615 .

Bracket all calls to save the game or high score fie with signals_protect(true) ... signals_protect(false):  save_game() does it automatically; save_game_checked(), savefile_save(), and enter_score() (via death_knowledge()) have to be manually bracketed.

src/ui-signals.c
@@ -36,6 +36,10 @@ int16_t signal_count;	/* Count interrupts ("I'm going to count to five") */
 
 typedef void (*Signal_Handler_t)(int);
 
+#ifdef SIGTSTP
+static Signal_Handler_t tstp_handler = SIG_DFL;
+#endif
+
 static int install_handler(int sig, Signal_Handler_t handler)
 {
 #ifdef HAVE_SIGACTION
@@ -68,49 +72,11 @@ static void handle_signal_disconnect(int sig)
 
 #ifdef SIGTSTP
 /**
- * Handle signals -- suspend
- *
- * Actually suspend the game, and then resume cleanly
+ * Handle signals -- request suspend
  */
 static void handle_signal_suspend(int sig)
 {
-	/* Protect errno from library calls in signal handler */
-	int save_errno = errno;
-
-#ifndef HAVE_SIGACTION
-	/* Disable handler */
-	(void)install_handler(sig, SIG_IGN);
-#endif
-
-#ifdef SIGSTOP
-
-	/* Flush output */
-	Term_fresh();
-
-	/* Suspend the "Term" */
-	Term_xtra(TERM_XTRA_ALIVE, 0);
-
-	/* Suspend ourself */
-	(void)kill(0, SIGSTOP);
-
-	/* Resume the "Term" */
-	Term_xtra(TERM_XTRA_ALIVE, 1);
-
-	/* Redraw the term */
-	Term_redraw();
-
-	/* Flush the term */
-	Term_fresh();
-
-#endif
-
-#ifndef HAVE_SIGACTION
-	/* Restore handler */
-	(void)install_handler(sig, handle_signal_suspend);
-#endif
-
-	/* Restore errno */
-	errno = save_errno;
+	terms_suspending = 1;
 }
 #endif /* ifdef SIGTSTP */
 
@@ -266,7 +232,7 @@ static void handle_signal_abort(int sig)
 	savefile_get_panic_name(panicfile, sizeof(panicfile), savefile);
 
 	/* Forbid suspend */
-	signals_ignore_tstp();
+	signals_protect(true);
 
 	/* Attempt to save */
 	if (panicfile[0] && savefile_save(panicfile))
@@ -282,29 +248,68 @@ static void handle_signal_abort(int sig)
 }
 
 
-
-
 /**
- * Ignore SIGTSTP signals (keyboard suspend)
+ * Temporarily modify whether certain signals have an effect.
+ *
+ * \param on will, if true, disable certain signals' effects.  If false, those
+ * signals' effects are restored.
+ *
+ * The signals affected are those that are typically user-initiated, could
+ * lead to denial of service (lock file left in place) or partially written
+ * important data, and, if blocked or ignored still allow for normal execution
+ * to proceed:  SIGINT, SIGQUIT, SIGTERM, and SIGTSTP.  Whether the signals
+ * are only blocked from delivery or ignored depends on what the platform
+ * allows.
  */
-void signals_ignore_tstp(void)
+void signals_protect(bool on)
 {
+#ifdef HAVE_SIGPROCMASK
+	sigset_t s;
 
-#ifdef SIGTSTP
-	(void)install_handler(SIGTSTP, SIG_IGN);
+	(void)sigemptyset(&s);
+#ifdef SIGINT
+	(void)sigaddset(&s, SIGINT);
 #endif
-
-}
-
-/**
- * Handle SIGTSTP signals (keyboard suspend)
- */
-void signals_handle_tstp(void)
-{
-
-#ifdef SIGTSTP
-	(void)install_handler(SIGTSTP, handle_signal_suspend);
+#ifdef SIGQUIT
+	(void)sigaddset(&s, SIGQUIT);
 #endif
+#ifdef SIGTERM
+	(void)sigaddset(&s, SIGTERM);
+#endif
+#ifdef SIGTSTP
+	(void)sigaddset(&s, SIGTSTP);
+#endif
+	(void)sigprocmask((on) ? SIG_BLOCK : SIG_UNBLOCK, &s, NULL);
+
+#else
+	if (on) {
+#ifdef SIGINT
+		(void)install_handler(SIGINT, SIG_IGN);
+#endif
+#ifdef SIGQUIT
+		(void)install_handler(SIGQUIT, SIG_IGN);
+#endif
+#ifdef SIGTERM
+		(void)install_handler(SIGTERM, SIG_IGN);
+#endif
+#ifdef SIGTSTP
+		(void)install_handler(SIGTSTP, SIG_IGN);
+#endif
+	} else {
+#ifdef SIGINT
+		(void)install_handler(SIGINT, handle_signal_simple);
+#endif
+#ifdef SIGQUIT
+		(void)install_handler(SIGQUIT, handle_signal_abort);
+#endif
+#ifdef SIGTERM
+		(void)install_handler(SIGTERM, handle_signal_simple);
+#endif
+#ifdef SIGTSTP
+		(void)install_handler(SIGTSTP, tstp_handler);
+#endif
+	}
+#endif /* else HAVE_SIGPROCMASK */
 
 }
 
@@ -312,7 +317,7 @@ void signals_handle_tstp(void)
 /**
  * Prepare to handle the relevant signals
  */
-void signals_init(bool hup_disconnects)
+void signals_init(bool hup_disconnects, bool tstp_default)
 {
 
 #ifdef SIGHUP
@@ -324,7 +329,10 @@ void signals_init(bool hup_disconnects)
 
 
 #ifdef SIGTSTP
-	(void)install_handler(SIGTSTP, handle_signal_suspend);
+	tstp_handler = (tstp_default) ? SIG_DFL : handle_signal_suspend;
+	(void)install_handler(SIGTSTP, tstp_handler);
+#else
+	(void)tstp_default;
 #endif
 
 
@@ -402,28 +410,55 @@ void signals_init(bool hup_disconnects)
 
 #else	/* !WINDOWS */
 
-
 /**
  * Do nothing
  */
-void signals_ignore_tstp(void)
+void signals_protect(bool on)
 {
+	(void)on;
 }
 
-/**
- * Do nothing
- */
-void signals_handle_tstp(void)
-{
-}
 
 /**
  * Do nothing
  */
-void signals_init(bool hup_disconnects)
+void signals_init(bool hup_disconnects, bool tstp_default)
 {
 	(void)hup_disconnects;
+	(void)tstp_default;
 }
 
 #endif	/* !WINDOWS */
 
+
+/**
+ * Suspend the user interface and then resume upon receiving SIGCONT.
+ */
+void signals_perform_deferred_suspend(void)
+{
+#ifdef SIGSTOP
+	/* Flush output */
+	Term_fresh();
+
+	/* Suspend the "Term" */
+	Term_xtra(TERM_XTRA_ALIVE, 0);
+
+	/* Suspend ourself */
+	(void)kill(0, SIGSTOP);
+
+	/* Clear the indicator. */
+	terms_suspending = 0;
+
+	/* Resume the "Term" */
+	Term_xtra(TERM_XTRA_ALIVE, 1);
+
+	/* Redraw the term */
+	Term_redraw();
+
+	/* Flush the term */
+	Term_fresh();
+#else
+	/* Clear the indicator. */
+	terms_suspending = 0;
+#endif
+}

acinclude.m4
@@ -96,13 +96,34 @@ int main(int argc, char *argv[]) {
 	a.sa_handler = SIG_IGN;
 	(void)sigemptyset(&a.sa_mask);
 	a.sa_flags = 0;
-	return sigaction(SIGINT, &a, 0);
+	return (sigaction(SIGINT, &a, 0) == 0) ? 0 : 1;
 }
 ]])],
 		[AC_MSG_RESULT([yes])
 			AC_DEFINE([HAVE_SIGACTION], [1], [System supports sigaction().])],
 		[AC_MSG_RESULT([no])])]
 )
+
+dnl Set HAVE_SIGPROCMASK if sigprocmask() is available.
+AC_DEFUN([MY_CHECK_SIGPROCMASK], [
+	AC_MSG_CHECKING([availability of sigprocmask()])
+	AC_LINK_IFELSE([AC_LANG_SOURCE([[
+#include <signal.h>
+int main(int argc, char *argv[]) {
+	sigset_t s1, s2;
+
+	(void)sigemptyset(&s1);
+	(void)sigaddset(&s1, SIGINT);
+	if (sigprocmask(SIG_BLOCK, &s1, 0) != 0) return 1;
+	if (sigprocmask(SIG_UNBLOCK, &s1, &s2) != 0) return 1;
+	return (sigprocmask(SIG_SETMASK, &s2, 0) == 0) ? 0 : 1;
+}
+]])],
+		[AC_MSG_RESULT([yes])
+			AC_DEFINE([HAVE_SIGPROCMASK], [1], [System supports sigprocmask().])],
+		[AC_MSG_RESULT([no])])]
+)
+
 
 
 # Configure paths for SDL2

configure.ac
@@ -97,6 +97,7 @@ AC_CHECK_HEADERS([fcntl.h])
 AC_HEADER_STDBOOL
 AC_CHECK_FUNCS([mkdir setresgid setegid stat])
 MY_CHECK_SIGACTION
+MY_CHECK_SIGPROCMASK
 
 dnl needed because h-basic.h checks for this define for autoconf support.
 CPPFLAGS="$CPPFLAGS -DHAVE_CONFIG_H"

src/h-basic.h
@@ -55,14 +55,17 @@
 # endif
 
 /**
- * May need to be tightened:  without autoconf.h assume all Unixes have mkdir()
- * and sigaction().
+ * May need to be tightened:  without autoconf.h assume all Unixes have mkdir(),
+ * sigaction(), and sigprocmask().
  */
 # if !defined(HAVE_MKDIR) && !defined(HAVE_CONFIG_H)
 #   define HAVE_MKDIR
 # endif
 # if !defined(HAVE_SIGACTION) && !defined(HAVE_CONFIG_H)
 #   define HAVE_SIGACTION 1
+# endif
+# if !defined(HAVE_SIGPROCMASK) && !defined(HAVE_CONFIG_H)
+#   define HAVE_SIGPROCMASK 1
 # endif
 
 #endif

src/main-gcu.c
@@ -24,6 +24,7 @@
 #include "ui-command.h"
 #include "ui-display.h"
 #include "ui-prefs.h"
+#include "ui-signals.h"
 
 #ifdef USE_GCU
 #include "main.h"
@@ -789,6 +790,7 @@ static void do_gcu_resize(void) {
 static errr Term_xtra_gcu_event(int v) {
 	int i, j, k, mods=0;
 
+	if (terms_suspending) signals_perform_deferred_suspend();
 	if (v) {
 		/* Wait for a keypress; use halfdelay(1) so if the user takes more */
 		/* than 0.2 seconds we get a chance to do updates. */
@@ -796,6 +798,11 @@ static errr Term_xtra_gcu_event(int v) {
 		i = getch();
 		while (i == ERR) {
 			if (terms_disconnecting) return 1;
+			if (terms_suspending) {
+				cbreak();
+				signals_perform_deferred_suspend();
+				halfdelay(2);
+			}
 			idle_update();
 			i = getch();
 		}

src/main-sdl.c
@@ -29,6 +29,7 @@
 #include "ui-map.h"
 #include "ui-output.h"
 #include "ui-prefs.h"
+#include "ui-signals.h"
 
 /**
  * Comments and suggestions are welcome. The UI probably needs some
@@ -138,6 +139,11 @@ static int full_h;
 
 /* Want fullscreen? */
 static bool fullscreen = false;
+/*
+ * If true, temporarily not in fullscreen mode because the application
+ * was stopped
+ */
+static bool withdrawn_fullscreen = false;
 
 static int overdraw = 0;
 static int overdraw_max = 0;
@@ -559,6 +565,7 @@ static int SelectedGfx;				/* Current selected gfx */
 static bool SimpleConfirm(const char *msg, const char *label1,
 		const char *label2, bool first_default);
 static int sdl_ModalEventLoop(void);
+static void perform_video_resize(bool immediate);
 
 /**
  * Verify if the given path refers to a font file that can be used.
@@ -3589,15 +3596,7 @@ static void AcceptChanges(sdl_Button *sender)
 		if (character_dungeon) do_cmd_redraw();
 
 	if (do_video_reset) {
-		SDL_Event Event;
-		
-		memset(&Event, 0, sizeof(SDL_Event));
-		
-		Event.type = SDL_VIDEORESIZE;
-		Event.resize.w = screen_w;
-		Event.resize.h = screen_h;
-		
-		SDL_PushEvent(&Event);
+		perform_video_resize(false);
 	}
 
 	do_update = false;
@@ -4716,6 +4715,21 @@ static void sdl_VideoResize(SDL_Event *e)
 	init_windows();
 }
 
+static void perform_video_resize(bool immediate)
+{
+	SDL_Event e;
+
+	(void)memset(&e, 0, sizeof(e));
+	e.type = SDL_VIDEORESIZE;
+	e.resize.w = screen_w;
+	e.resize.h = screen_h;
+	if (immediate) {
+		sdl_VideoResize(&e);
+	} else {
+		SDL_PushEvent(&e);
+	}
+}
+
 /**
  * Handle a single message sent to the application.
  *
@@ -4974,6 +4988,9 @@ static errr Term_xtra_sdl_event(int v)
 	SDL_Event event;
 	errr error = 0;
 
+	if (terms_suspending) {
+		signals_perform_deferred_suspend();
+	}
 	if (v) {
 		/* Wait in 0.02s increments while updating animations every 0.2s */
 		int i = 0;
@@ -4982,6 +4999,9 @@ static errr Term_xtra_sdl_event(int v)
 			if (i == 0) idle_update();
 			usleep(20000);
 			i = (i + 1) % 10;
+			if (terms_suspending) {
+				signals_perform_deferred_suspend();
+			}
 		}
 
 		/* Handle it */
@@ -5143,7 +5163,31 @@ static errr Term_xtra_sdl(int n, int v)
 				text_colours[i].g = angband_color_table[i][2];
 				text_colours[i].b = angband_color_table[i][3];
 			}
+			return 0;
 		}
+		case TERM_XTRA_ALIVE:
+			if (v) {
+				/*
+				 * Resuming:  if we were fullscreen go back
+				 * to that
+				 */
+				if (withdrawn_fullscreen) {
+					fullscreen = true;
+					withdrawn_fullscreen = false;
+					perform_video_resize(false);
+				}
+			} else {
+				/*
+				 * Suspending:  if we are fullscreen, withdraw
+				 * from that
+				 */
+				if (fullscreen) {
+					fullscreen = false;
+					withdrawn_fullscreen = true;
+					perform_video_resize(true);
+				}
+			}
+			return 0;
 	}
 
 	return (1);

src/main-sdl2.c
@@ -47,6 +47,7 @@
 #include "ui-map.h"
 #include "ui-output.h"
 #include "ui-prefs.h"
+#include "ui-signals.h"
 #include "ui-term.h"
 
 #define MAX_SUBWINDOWS \
@@ -352,6 +353,11 @@ struct sdlpui_window {
 
 	/** window has changed and must be redrawn */
 	bool dirty;
+	/**
+	 * when true, window is temporarily not in fullscreen mode because the
+	 * application was stopped
+	 */
+	bool withdrawn_fullscreen;
 
 	/** limiter for frames */
 	Uint32 next_redraw;
@@ -2580,72 +2586,73 @@ static struct sdlpui_dialog *handle_menu_windows(struct sdlpui_control *ctrl,
 	return result;
 }
 
-static void handle_menu_fullscreen(struct sdlpui_control *ctrl,
-		struct sdlpui_dialog *dlg, struct sdlpui_window *window)
+static bool toggle_fullscreen(struct sdlpui_window *window)
 {
 	bool was_fullscreen = (window->flags & SDL_WINDOW_FULLSCREEN_DESKTOP);
 	SDL_Rect tmp_rect;
-
-	sdlpui_popdown_dialog(dlg, window, SDL_TRUE);
+	size_t i;
 
 	SDL_GetWindowSize(window->window, &tmp_rect.w, &tmp_rect.h);
 	SDL_GetWindowPosition(window->window, &tmp_rect.x, &tmp_rect.y);
-	if (!SDL_SetWindowFullscreen(window->window, (was_fullscreen) ?
+
+	if (SDL_SetWindowFullscreen(window->window, (was_fullscreen) ?
 			0 : SDL_WINDOW_FULLSCREEN_DESKTOP)) {
-		/* Succeeded.  Swap cached sizes. */
-		size_t i;
+		return false;
+	}
 
-		window->full_rect = window->stored_rect;
-		window->stored_rect = tmp_rect;
-		for (i = 0; i < N_ELEMENTS(window->subwindows); ++i) {
-			struct subwindow *subwindow = window->subwindows[i];
+	/* Succeeded.  Swap cached sizes. */
+	window->full_rect = window->stored_rect;
+	window->stored_rect = tmp_rect;
+	for (i = 0; i < N_ELEMENTS(window->subwindows); ++i) {
+		struct subwindow *subwindow = window->subwindows[i];
 
-			if (subwindow != NULL) {
-				tmp_rect = subwindow->stored_rect;
-				subwindow->stored_rect = subwindow->full_rect;
-				subwindow->full_rect = tmp_rect;
-				if (!subwindow->full_rect.w
-						|| !subwindow->full_rect.h) {
-					/*
-					 * Nothing configured so far for this
-					 * mode, so use the configuration from
-					 * the other mode.
-					 */
-					subwindow->full_rect =
-						subwindow->stored_rect;
-				} else if (subwindow->full_rect.w
-						!= subwindow->stored_rect.w
-						|| subwindow->full_rect.h
-						!= subwindow->stored_rect.h) {
-					subwindow->sizing_rect =
-						subwindow->full_rect;
-					resize_subwindow(subwindow);
-				}
+		if (subwindow != NULL) {
+			tmp_rect = subwindow->stored_rect;
+			subwindow->stored_rect = subwindow->full_rect;
+			subwindow->full_rect = tmp_rect;
+			if (!subwindow->full_rect.w
+					|| !subwindow->full_rect.h) {
+				/*
+				 * Nothing configured so far for this mode, so
+				 * use the configuration from the other mode.
+				 */
+				subwindow->full_rect = subwindow->stored_rect;
+			} else if (subwindow->full_rect.w
+					!= subwindow->stored_rect.w
+					|| subwindow->full_rect.h
+					!= subwindow->stored_rect.h) {
+				subwindow->sizing_rect = subwindow->full_rect;
+				resize_subwindow(subwindow);
 			}
 		}
+	}
+	if (was_fullscreen) {
+		int minw, minh;
 
-		if (was_fullscreen) {
-			int minw, minh;
-
-			get_minimum_window_size(window, &minw, &minh);
-			SDL_SetWindowMinimumSize(window->window, minw, minh);
-			/*
-			 * If there is a previously configured size, use it.
-			 * Otherwise, rely on SDL's default behavior.
-			 */
-			if (window->full_rect.w && window->full_rect.h) {
-				SDL_SetWindowSize(window->window,
-					window->full_rect.w,
-					window->full_rect.h);
-				resize_window(window, window->full_rect.w,
-					window->full_rect.h);
-				SDL_SetWindowPosition(window->window,
-					window->full_rect.x,
-					window->full_rect.y);
-			}
+		get_minimum_window_size(window, &minw, &minh);
+		SDL_SetWindowMinimumSize(window->window, minw, minh);
+		/*
+		 * If there is a previously configured size, use it.
+		 * Otherwise, rely on SDL's default behavior.
+		 */
+		if (window->full_rect.w && window->full_rect.h) {
+			SDL_SetWindowSize(window->window, window->full_rect.w,
+				window->full_rect.h);
+			resize_window(window, window->full_rect.w,
+				window->full_rect.h);
+			SDL_SetWindowPosition(window->window,
+				window->full_rect.x, window->full_rect.y);
 		}
-		window->flags = SDL_GetWindowFlags(window->window);
-	} else {
+	}
+	window->flags = SDL_GetWindowFlags(window->window);
+	return true;
+}
+
+static void handle_menu_fullscreen(struct sdlpui_control *ctrl,
+		struct sdlpui_dialog *dlg, struct sdlpui_window *window)
+{
+	sdlpui_popdown_dialog(dlg, window, SDL_TRUE);
+	if (!toggle_fullscreen(window)) {
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING,
 			"Fullscreen failure",
 			format("Could not change fullscreen setting:\n%s",
@@ -4730,6 +4737,9 @@ static errr term_xtra_event(int v)
 	if (v) {
 		while (true) {
 			for (int i = 0; i < DEFAULT_IDLE_UPDATE_PERIOD; i++) {
+				if (terms_suspending) {
+					signals_perform_deferred_suspend();
+				}
 				if (get_event(subwindow->app)) {
 					return 0;
 				}
@@ -4741,6 +4751,9 @@ static errr term_xtra_event(int v)
 			idle_update();
 		}
 	} else {
+		if (terms_suspending) {
+			signals_perform_deferred_suspend();
+		}
 		(void) get_event(subwindow->app);
 	}
 
@@ -4804,6 +4817,37 @@ static errr term_xtra_react(void)
 	return 0;
 }
 
+static errr term_xtra_alive(int v)
+{
+	unsigned i;
+
+	if (v) {
+		/* Resuming:  if we were fullscreen, go back to that */
+		for (i = 0; i < MAX_WINDOWS; ++i) {
+			struct sdlpui_window *w = get_window_direct(&g_app, i);
+
+			if (w && w->window && w->withdrawn_fullscreen) {
+				if (toggle_fullscreen(w)) {
+					w->withdrawn_fullscreen = false;
+				}
+			}
+		}
+	} else {
+		/* Suspending:  if we are fullscreen, withdraw from that */
+		for (i = 0; i < MAX_WINDOWS; ++i) {
+			struct sdlpui_window *w = get_window_direct(&g_app, i);
+
+			if (w && w->window && (w->flags
+					& SDL_WINDOW_FULLSCREEN_DESKTOP)) {
+				if (toggle_fullscreen(w)) {
+					w->withdrawn_fullscreen = true;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
 static errr term_xtra_hook(int n, int v)
 {
 	switch (n) {
@@ -4819,6 +4863,8 @@ static errr term_xtra_hook(int n, int v)
 			return term_xtra_fresh();
 		case TERM_XTRA_REACT:
 			return term_xtra_react();
+		case TERM_XTRA_ALIVE:
+			return term_xtra_alive(v);
 		default:
 			return 0;
 	}
@@ -6362,6 +6408,7 @@ static void wipe_window(struct sdlpui_window *window, int display)
 	window->graphics.id = GRAPHICS_NONE;
 
 	window->dirty = true;
+	window->withdrawn_fullscreen = false;
 
 	window->config = NULL;
 	window->inited = true;

src/main.c
@@ -64,35 +64,35 @@
 static const struct module modules[] =
 {
 #ifdef USE_X11
-	{ "x11", help_x11, init_x11, false },
+	{ "x11", help_x11, init_x11, false, true },
 #endif /* USE_X11 */
 
 #ifdef USE_SDL
-	{ "sdl", help_sdl, init_sdl, false },
+	{ "sdl", help_sdl, init_sdl, false, false },
 #endif /* USE_SDL */
 
 #ifdef USE_SDL2
-	{ "sdl2", help_sdl2, init_sdl2, false },
+	{ "sdl2", help_sdl2, init_sdl2, false, false },
 #endif /* USE_SDL2 */
 
 #ifdef USE_GCU
-	{ "gcu", help_gcu, init_gcu, true },
+	{ "gcu", help_gcu, init_gcu, true, false },
 #endif /* USE_GCU */
 
 #ifdef USE_TEST
-	{ "test", help_test, init_test, false },
+	{ "test", help_test, init_test, false, true },
 #endif /* !USE_TEST */
 
 #ifdef USE_STATS
-	{ "stats", help_stats, init_stats, false },
+	{ "stats", help_stats, init_stats, false, true },
 #endif /* USE_STATS */
 
 #ifdef USE_SPOIL
-	{ "spoil", help_spoil, init_spoil, false },
+	{ "spoil", help_spoil, init_spoil, false, true },
 #endif
 
 #ifdef USE_IBM
-	{ "ibm", help_ibm, init_ibm, false },
+	{ "ibm", help_ibm, init_ibm, false, true },
 #endif /* USE_IBM */
 };
 
@@ -545,7 +545,7 @@ int main(int argc, char *argv[])
 	if (!done) quit("Unable to prepare any 'display module'!");
 
 	/* Catch nasty signals */
-	signals_init(modules[i].hup_disconnects);
+	signals_init(modules[i].hup_disconnects, modules[i].tstp_default);
 
 	/* Set up the command hook */
 	cmd_get_hook = textui_get_cmd;

backwardsEric/angband

commit fbd42279d7ceaa1b1a9589b920595fd4b26408aa

Change handlers for SIGTERM and SIGQUIT
Consistently use handle_signal_simple() for SIGTERM:  attempt to gracefully exit.  Consistently use handle_signal_abort() for SIGQUIT:  exit at the point the signal was received and indicate that the exit was due to the signal.  Related to https://github.com/angband/angband/issues/6586 .  Corrects regression in https://github.com/angband/angband/commit/e4ada7b1410f1862deb43fe08fadf4f6f773d24f when signals_protect() has to reinstall handlers for SIGTERM and SIGQUIT.

src/ui-signals.c
@@ -364,7 +364,7 @@ void signals_init(bool hup_disconnects, bool tstp_default)
 #endif
 
 #ifdef SIGQUIT
-	(void)install_handler(SIGQUIT, handle_signal_simple);
+	(void)install_handler(SIGQUIT, handle_signal_abort);
 #endif
 
 
@@ -396,7 +396,7 @@ void signals_init(bool hup_disconnects, bool tstp_default)
 #endif
 
 #ifdef SIGTERM
-	(void)install_handler(SIGTERM, handle_signal_abort);
+	(void)install_handler(SIGTERM, handle_signal_simple);
 #endif
 
 #ifdef SIGPIPE